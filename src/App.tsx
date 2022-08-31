6a86754f484e5405f2d2046733cc167b6bf07641
1freshie <hristian.todorov14@gmail.com>
2022-08-31 23:04:17 +0300

    fixed folders and files, added some design, added full functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
+
 import './App.css';
 
-const App = () => {
+const App: React.FunctionComponent = () => {
+  const [advice, setAdvice] = useState({
+    id: 0,
+    text: '',
+  });
+  const [isLoading, setIsLoading] = useState(true);
+
+  const generateAdviceHandler = async () => {
+    try {
+      const response = await fetch('https://api.adviceslip.com/advice');
+
+      if (!response.ok) {
+        throw new Error("Couldn't load advice");
+      }
+
+      const responseData = await response.json();
+      const loadedAdvice = {
+        id: responseData.slip.id,
+        text: responseData.slip.advice,
+      };
+
+      console.log(responseData);
+
+      setAdvice(loadedAdvice);
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="card">
+        {!isLoading ? (
+          <p className="advice__id">ADVICE #{advice.id}</p>
+        ) : (
+          <p className="advice__id">...</p>
+        )}
+        {!isLoading ? (
+          <p className="advice__text">"{advice.text}"</p>
+        ) : (
+          <p className="advice__text">Loading...</p>
+        )}
+        <div className='advice__divider'></div>
+      </div>
+        <button className="advice__button" onClick={generateAdviceHandler}>
+        NEW ADVICE
+      </button>
+      {/* <div className="advice__button">ASDASD</div> */}
     </div>
   );
 };
